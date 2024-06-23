74f7971853f450cafd6c30e76297127260426447
Sumedh Natu <73115797+Pune007@users.noreply.github.com>
2024-06-23 22:23:29 +0530

    Create VFXPlayer.cs

---
+++ VFXPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXPlayer : MonoBehaviour
+{
+    [SerializeField] protected Animator anim;
+    [SerializeField] protected SpriteRenderer sprite;
+
+    [Space]
+
+    [SerializeField] protected AudioClip[] soundEffects;
+    [SerializeField] protected AudioSource audioSource;
+
+    protected string CurrentState;
+    protected string PreviousState;
+
+    public void SetState(string state)
+    {
+        if (CurrentState == state)
+            return;
+
+        PreviousState = CurrentState;
+
+        CurrentState = state;
+
+        anim.SetBool(state, true);
+        anim.SetBool(PreviousState, false);
+    }
+
+    public void PlayAudio(string clipName)
+    {
+        foreach(AudioClip aClip in soundEffects)
+        {
+            if (aClip.name == clipName)
+            {
+                audioSource.clip = aClip;
+                audioSource.Play();
+            }
+        }
+    }
+}
